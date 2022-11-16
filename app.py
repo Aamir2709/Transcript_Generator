da0256f55b823bcd544ddaa1674711f658a8cc83
Aamir2709 <aamirbaugwala@gmail.com>
2022-11-16 23:25:14 +0530

    first commits

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask,request,jsonify,render_template
+app = Flask(__name__,template_folder='template')
+import numpy as np
+import pandas as pd
+import pickle
+from flask_cors import CORS,cross_origin
+import requests,bs4
+import utils
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/generate',methods=['POST'])
+def generate():
+    utube_link = str(request.form.get('email'))
+    result = utils.link(utube_link)
+    return render_template("index.html",result=result)
+
+@app.route('/summarizer')
+def summarizer():
+    return render_template("summerizer.html")
+
+@app.route('/get_summarized',methods=['POST'])
+def generate_summarizer():
+    utube_link = str(request.form.get('email'))
+    result = utils.summarized(utube_link)
+    return render_template("summerizer.html",result=result)
+
+
+
+
+if __name__=="__main__":
+    app.run()
---
+++ utils.py
@@ -0,0 +1,43 @@
+from transformers import pipeline
+from youtube_transcript_api import YouTubeTranscriptApi
+from IPython.display import YouTubeVideo
+
+def link(utube_link):
+    video_id_list = utube_link.split("=")
+    video_id = video_id_list[1]
+    try:
+        transcript = YouTubeTranscriptApi.get_transcript(video_id)
+        result = ""
+        for i in transcript:
+            result += ' ' + i['text']
+        return result
+    except:
+        return "Sorry there was an issue,Please check if the video have subtitle/captions turned on"
+
+
+def summarized(utube_link):
+    video_id_list = utube_link.split("=")
+    video_id = video_id_list[1]
+    try:
+        transcript = YouTubeTranscriptApi.get_transcript(video_id)
+        result = ""
+        for i in transcript:
+            result += ' ' + i['text']
+        summarizer = pipeline('summarization')
+        num_iters = int(len(result)/1000)
+        summarized_text = []
+        for i in range(0, num_iters + 1):
+            start = 0
+            start = i * 1000
+            end = (i + 1) * 1000
+            #print("input text \n" + result[start:end])
+            out = summarizer(result[start:end])
+            out = out[0]
+            out = out['summary_text']
+            #print("Summarized text\n"+out)
+            summarized_text.append(out)
+        return summarized_text
+    except:
+        return "Sorry there was an issue"
+    
+#print(link("https://www.youtube.com/watch?v=A4OmtyaBHFE"))
