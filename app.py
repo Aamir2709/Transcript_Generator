c12cbcf8416078db2a7540bd9637eb78cd374680
Aamir2709 <84448909+Aamir2709@users.noreply.github.com>
2022-11-17 00:35:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,request,jsonify,render_template
+from flask import Flask,request,render_template
 app = Flask(__name__,template_folder='template')
 import numpy as np
 import pandas as pd
