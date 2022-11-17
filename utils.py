7cddaff386aa1fca6bc8129b6a459c53fc070a6e
Aamir2709 <84448909+Aamir2709@users.noreply.github.com>
2022-11-17 13:50:07 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -3,9 +3,18 @@ from youtube_transcript_api import YouTubeTranscriptApi
 from IPython.display import YouTubeVideo
 
 def link(utube_link):
-   if "=" not in utube_link:
-        return "Sorry there was an issue,Please check if the video have subtitle/captions turned on"
-   else:
+    if "=" not in utube_link:
+        video_id_list = utube_link.split("/")
+        video_id = video_id_list[3]
+        try:
+            transcript = YouTubeTranscriptApi.get_transcript(video_id)
+            result = ""
+            for i in transcript:
+                result += ' ' + i['text']
+            return result
+        except:
+            return "Sorry there was an issue,Please check if the video have subtitle/captions turned on"
+    else:
         video_id_list = utube_link.split("=")
         video_id = video_id_list[1]
         try:
